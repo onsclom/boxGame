c5e48abe8b2474ec32550fee99cc8e0aa4031bb7
Austin Merrick <austinmerrick98@gmail.com>
2020-08-07 10:32:10 -0700

    Animations and sounds

---
+++ Character.gd
@@ -0,0 +1,179 @@
+extends KinematicBody2D
+
+const MOVE_SPEED = 75
+const JUMP_FORCE = 200
+const GRAVITY = 10
+const MAX_FALL_SPEED = 200
+const MAX_SLIDE_SPEED = 35
+
+
+var y_velo = 0
+var facing_right = false
+var coyoteTime = .1
+
+var wallJumpVel = 0
+var wallJumpTime = 0
+var wallJumpPenaltyTime = .355
+var jumpRememberTime = .2
+
+var timeSinceGrounded = 0
+var jumpedLast = jumpRememberTime
+
+
+onready var sprite = $AnimatedSprite
+var rotationAmount = 15
+var rotationDir = 0
+var rotationChangeSpeed = 1
+var rotationGoal = 0
+
+var maxFallSpeed = 200
+var maxSquish = .65
+
+var prevYvelo = 0
+
+func _ready():
+	#AudioServer.set_bus_mute(AudioServer.get_bus_index("Master"), false)
+	pass 
+	
+func _physics_process(delta):
+	
+	var grounded = is_on_floor()
+	
+	var move_dir = 0
+	if Input.is_action_pressed("move_right"):
+		move_dir += 1
+	if Input.is_action_pressed("move_left"):
+		move_dir -= 1
+	
+	if grounded and move_dir != 0:
+		$AnimatedSprite.play("walking")
+		if !$Walking.playing:
+			$Walking.play()
+	elif timeSinceGrounded > .05 or move_dir == 0:
+		$Walking.stop()
+		$AnimatedSprite.play("default")
+		
+	rotationGoal = move_dir*rotationAmount
+	$AnimatedSprite.rotation_degrees = rotationGoal
+	
+	if wallJumpTime < wallJumpPenaltyTime and grounded == false and wallJumpVel != 0:
+		move_dir = wallJumpVel
+	 
+	var result = move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
+	
+	y_velo = result.y
+	if result.x == 0 and wallJumpVel != 0:
+		wallJumpVel = 0
+	
+	if timeSinceGrounded > .05:
+		$AnimatedSprite.play("jumping")
+		
+	
+	if (!grounded and timeSinceGrounded ==0) or timeSinceGrounded > 0:
+		timeSinceGrounded += delta
+	if grounded:
+		timeSinceGrounded = 0
+	
+	y_velo += GRAVITY
+
+	if (grounded or timeSinceGrounded < coyoteTime) and (Input.is_action_just_pressed("jump") or jumpedLast < jumpRememberTime):
+		$JumpParticles.restart()
+		$JumpParticles.emitting = true
+		y_velo = -JUMP_FORCE
+		timeSinceGrounded = coyoteTime
+		jumpedLast = jumpRememberTime
+		$Jumping.playing = true
+	elif ($Left.is_colliding() or $Left2.is_colliding()) and (Input.is_action_just_pressed("jump") or jumpedLast < jumpRememberTime):
+		$LeftWallJump.restart()
+		$LeftWallJump.emitting=true
+		y_velo = -JUMP_FORCE
+		wallJumpVel = 1
+		wallJumpTime = 0
+		timeSinceGrounded = coyoteTime
+		jumpedLast = jumpRememberTime
+		$Jumping.playing = true
+		
+	elif ($Right.is_colliding() or $Right2.is_colliding()) and (Input.is_action_just_pressed("jump") or jumpedLast < jumpRememberTime):
+		$RightWallJump.restart()
+		$RightWallJump.emitting=true
+		y_velo = -JUMP_FORCE
+		wallJumpVel = -1
+		wallJumpTime = 0
+		timeSinceGrounded = coyoteTime
+		jumpedLast = jumpRememberTime
+		$Jumping.playing = true
+		
+	elif Input.is_action_just_pressed("jump"):
+		jumpedLast = 0
+		timeSinceGrounded = coyoteTime
+		
+	jumpedLast += delta
+		
+	wallJumpTime += delta
+		
+#	if ($Left.is_colliding() or $Left2.is_colliding()) and wallJumpVel == -1:
+#		wallJumpVel = 0
+#	elif ($Right.is_colliding() or $Right2.is_colliding()) and wallJumpVel == 1:
+#		wallJumpVel = 0
+	if grounded:
+		wallJumpVel = 0
+		
+		
+	if y_velo <= 0 and Input.is_action_just_released("jump"):
+		y_velo /= 2.0
+		pass
+	if grounded and y_velo >= 1:
+		y_velo = 1
+	if y_velo > MAX_FALL_SPEED:
+		y_velo = MAX_FALL_SPEED
+		
+	var scaleAmount = max( range_lerp(y_velo, 0, 200, 1, maxSquish), .8)
+	$AnimatedSprite.scale.x = scaleAmount
+	
+	$LeftParticles.emitting = false
+	$RightParticles.emitting = false
+	if !grounded and timeSinceGrounded>.05 and Input.is_action_pressed("move_left"):
+		if $Left.is_colliding() or $Left2.is_colliding():
+				$LeftParticles.emitting = true
+				y_velo = min(y_velo, MAX_SLIDE_SPEED)
+				
+				if $Sliding.stream_paused and y_velo >= MAX_SLIDE_SPEED:
+					$Sliding.stream_paused = false
+		else:
+			if not $Sliding.stream_paused:
+					$Sliding.stream_paused = true
+	elif !grounded and timeSinceGrounded>.05 and Input.is_action_pressed("move_right"):
+		if $Right.is_colliding() or $Right2.is_colliding():
+				$RightParticles.emitting = true
+				y_velo = min(y_velo, MAX_SLIDE_SPEED)
+				
+				if $Sliding.stream_paused and y_velo >= MAX_SLIDE_SPEED:
+					$Sliding.stream_paused = false
+		else:
+			if not $Sliding.stream_paused:
+					$Sliding.stream_paused = true
+	else:
+		if not $Sliding.stream_paused:
+					$Sliding.stream_paused = true
+				
+	if prevYvelo>= 100 and ( $GroundCheck.is_colliding() or $GroundCheck2.is_colliding() ):
+		$JumpParticles.restart()
+		$JumpParticles.emitting = true	
+		if not $Landing.playing:
+			$Landing.play()
+			
+	prevYvelo = y_velo
+	
+	$WalkParticles.emitting = false
+	if move_dir != 0 and (grounded):
+		$WalkParticles.emitting = true
+		
+				
+	
+#	if grounded:
+#		if move_dir == 0:
+#			play_anim("idle")
+#		else:
+#			play_anim("walk")
+#	else:
+#		play_anim("jump")
---
+++ ColorChanger.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var firstMod = $CanvasModulate
+onready var secondMod = get_node("UI stuff/CanvasModulate")
+
+export var sat = .6
+export var v = 1
+var h
+
+var time = 0
+var timeSinceGrounded = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	time += delta
+	var newColor = Color.from_hsv(sin(time/8.0)/2.0+.5, sat, v )
+	firstMod.color = newColor
+	secondMod.color = newColor
